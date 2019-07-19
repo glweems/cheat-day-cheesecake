7d303e34661d5507aa5e84bebc656a684fa1081d
Garrett Weems <gwgraphicdesign@gmail.com>
2019-07-19 19:21:45 -0400

    💄 Button styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,7 @@ import { Flex } from './Styled';
 
 const Navbar = ({ toggleMenu }: { toggleMenu: any }) => (
   <Flex className="navbar" justifyContent="space-between" alignItems="center">
-    <button type="button" onClick={toggleMenu}>
-      <i className="fas fa-bars" />
-    </button>
+    <button type="button" className="btn fas fa-bars" onClick={toggleMenu} />
 
     <h3>Cheat Day Cheesecake</h3>
 
